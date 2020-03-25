5dbe75533f4e3ff3295a8cccf7d911f0e1a72bfb
Brenda <brendabdg000@gmail.com>
2020-03-25 19:50:14 -0300

    Adicionados arquivos php de projeto e fluxo

---
+++ add-projeto.php
@@ -0,0 +1,15 @@
+<?php
+	require_once 'config.php'; // Inclui o arquivo de configurações do site
+	include(HEADER_TEMPLATE);  // Inclui o header da página
+?>
+<!--Conteudo-->
+<div>
+
+
+
+
+</div>
+
+<?php
+	include(FOOTER_TEMPLATE); // Inclui o rodapé da página
+?>
